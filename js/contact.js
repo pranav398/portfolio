f5d5ad392caa0053254ed01eff67e349d010eb63
Pranav Bagrecha <pranavb030908@gmail.com>
2026-08-12 23:21:01 +0530

    v2.0
    Version 2.0 - dt. 12.08.2026

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
-const PUBLIC_KEY = "<YOUR_PUBLIC_KEY>";
-const SERVICE_ID = "<YOUR_SERVICE_ID>";
-const TEMPLATE_ID = "<YOUR_TEMPLATE_ID>";
+const PUBLIC_KEY = "uHXdG52lSYsVcY0P2";
+const SERVICE_ID = "service_ixk6z5p";
+const TEMPLATE_ID = "template_z52td9i";
 
 emailjs.init(PUBLIC_KEY);
 
